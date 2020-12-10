b192fa255f262ae1b4df037464ac40b82a34a3cd
Charl Cater <4677572+charlcater@users.noreply.github.com>
2020-12-10 12:10:35 +0200

    clean up day 04
    Cleaned up algorithm to not be such a huge nested mess

--- day_04/aoc2020_04.py
+++ day_04/aoc2020_04.py
@@ -20,7 +20,7 @@ def part1(entries):
 
 def part2(entries):
 
-    valid = 0
+    correct = 0
 
     for entry in entries:
         d = {}
@@ -29,27 +29,36 @@ def part2(entries):
             (key, val) = field.split(':')
             d[key] = val
 
-        if (len(d) == 8) or (len(d) == 7 and 'cid' not in d):
-            if (1920 <= int(d['byr']) <= 2002):
-                # print('byr: {}'.format(d.get('byr')))
-                if (2010 <= int(d['iyr']) <= 2020):
-                    # print('iyr: {}'.format(d.get('iyr')))
-                    if (2020 <= int(d['eyr']) <= 2030):
-                        # print('eyr: {}'.format(d.get('eyr')))
-                        if ( 'cm' in d['hgt'] and ( 150 <= int(d['hgt'].strip('cm')) <= 193)) \
-                            or ( 'in' in d['hgt'] and ( 59 <= int(d['hgt'].strip('in')) <= 76)):
-                            # print('hgt: {}'.format(d.get('hgt')))
-                            if (d['hcl'].strip('#').isalnum() and len(d['hcl']) == 7):
-                                # print('hcl: {}'.format(d.get('hcl'))
-                                if (d['ecl'] in eyecol):
-                                    # print('ecl: {}'.format(d.get('ecl')))
-                                    if (len(d['pid']) == 9):
-                                        # print('pid: {}'.format(d.get('pid')))
-                                        valid += 1
-        else:
-            pass
+        valid = True
 
-    return valid
+        if not ((len(d) == 8) or (len(d) == 7 and 'cid' not in d)):
+            valid = False
+        if 'byr' in d and not (1920 <= int(d['byr']) <= 2002):
+            valid = False
+            # print('byr: {}'.format(d.get('byr')))
+        if 'iyr' in d and not (2010 <= int(d['iyr']) <= 2020):
+            # print('iyr: {}'.format(d.get('iyr')))
+            valid = False
+        if 'eyr' in d and not (2020 <= int(d['eyr']) <= 2030):
+            # print('eyr: {}'.format(d.get('eyr')))
+            valid = False
+        if 'hgt' in d  and not (( 'cm' in d['hgt'] and ( 150 <= int(d['hgt'].strip('cm')) <= 193)) or ( 'in' in d['hgt'] and ( 59 <= int(d['hgt'].strip('in')) <= 76))):
+            # print('hgt: {}'.format(d.get('hgt')))
+            valid = False
+        if 'hcl' in d and not (d['hcl'].strip('#').isalnum() and len(d['hcl']) == 7):
+            # print('hcl: {}'.format(d.get('hcl')))
+            valid = False
+        if 'ecl' in d and not (d['ecl'] in eyecol):
+            # print('ecl: {}'.format(d.get('ecl')))
+            valid = False
+        if 'pid' in d and not (len(d['pid']) == 9):
+            # print('pid: {}'.format(d.get('pid')))
+            valid = False
+
+        if valid == True:
+            correct += 1
+
+    return correct
 
 entries = [entry.strip('\n') for entry in open("input.txt", "r").read().split('\n\n')]
 
