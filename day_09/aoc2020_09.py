fdf25b17c55e268311ba2c7985e23a0ce191d1b3
Charl Cater <4677572+charlcater@users.noreply.github.com>
2020-12-17 14:18:39 +0200

    Day 09

---
+++ day_09/aoc2020_09.py
@@ -0,0 +1,57 @@
+# Advent of Code 2020
+# --- Day 9: Encoding Error ---
+
+def part1(numlist):
+
+    i = 0
+    low = 0
+    high = 25
+
+    for number in numlist[high:]:
+        for n in numlist[low+i:high+i]:
+            valid = 0
+            nn = number - n
+
+            if nn > 0 and nn in numlist[low+i:high+i]:
+                # print('{} is valid'.format(number))
+                valid = 1
+                i += 1
+                break
+            else:
+                pass
+            
+        if valid == 0:
+            # print('invalid: {}'.format(number))
+            return(number)
+
+
+def part2(number, numbers):
+
+    maxlen = len(numbers)
+    i = 0 # index in input list
+
+    while i < maxlen-1:
+        doSum = True
+        j = 1
+        total = 0
+        while doSum:
+            # print(numbers[i:i+j])
+            total = sum(x for x in numbers[i:i+j])
+            # print(total)
+
+            if total == number:
+                # minval = max(numbers[i:i+j])
+                # maxval = min(numbers[i:i+j])
+                # print(minval, maxval)
+                return(min(numbers[i:i+j]) + max(numbers[i:i+j]))
+            elif total < number:
+                j += 1
+            else:
+                i += 1
+                doSum = False
+
+
+numlist = [int(entry.strip('\n')) for entry in open("input.txt", "r").readlines()]
+number = part1(numlist)
+print('Part 1: first invalid number = {}'.format(number))
+print('Part 2: encryption weakness = {}'.format(part2(number, numlist)))
