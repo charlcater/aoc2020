c3d498e46fa558a64eca2fbe401ee2e971bdac5d
Charl Cater <4677572+charlcater@users.noreply.github.com>
2020-12-19 12:11:20 +0200

    Day 1: much better, more efficient solution

--- day_01/aoc2020_01.py
+++ day_01/aoc2020_01.py
@@ -3,37 +3,62 @@
 
 def part1(lines):
 
-    count = 0
-    lineslen = len(lines)
+    # quadratic time, O(n^2)
+    # count = 0
+    # lineslen = len(lines)
     
-    for num1 in lines:
-        count += 1
+    # for num1 in lines:
+    #     count += 1
 
-        for num2 in lines[count:lineslen]:
-            sum = num1 + num2
+    #     for num2 in lines[count:lineslen]:
+    #         sum = num1 + num2
 
-            if sum == 2020:
-                product = num1 * num2
-                return product
+    #         if sum == 2020:
+    #             product = num1 * num2
+    #             return product
+
+    # linear time, O(n)
+    compls  = set()
+    for x in lines:
+        y = 2020 - x
+
+        if x in compls:
+            return  x * y
+
+        compls.add(y)
 
 
 def part2(lines):
 
-    count = 0
-    lineslen = len(lines)
+    # Cubic time (!), O(n^3)
+    # count = 0
+    # lineslen = len(lines)
 
-    for num1 in lines:
-        count += 1
+    # for num1 in lines:
+    #     count += 1
 
-        for num2 in lines[count:lineslen]:
+    #     for num2 in lines[count:lineslen]:
 
-            for num3 in lines[count+1:lineslen]:
-                sum = num1 + num2 + num3
+    #         for num3 in lines[count+1:lineslen]:
+    #             sum = num1 + num2 + num3
 
-                if sum == 2020:
-                    # print('Found it: {} + {} + {}'.format(num1, num2, num3))
-                    product = num1 * num2 * num3
-                    return product
+    #             if sum == 2020:
+    #                 # print('Found it: {} + {} + {}'.format(num1, num2, num3))
+    #                 product = num1 * num2 * num3
+    #                 return product
+
+    # quatratic time, O(n^2)
+    for i, x in enumerate(lines):
+        compls = set()
+        yz = 2020 - x
+
+        for y in lines[i + 1:]:
+            z = yz - y
+
+            if y in compls:
+                return x * y * z
+
+            compls.add(z)
 
 
 lines = [int(line.strip()) for line in open("input.txt", "r").readlines()]
